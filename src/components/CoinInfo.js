89597d6b370ab6f22b7f775e65e0c85ea882c8c6
rafael <rafaelreyesgarcia93@gmail.com>
2023-02-15 16:11:16 -0600

    can't figure out: react-chartjs-2 html-react-parser

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { AppState } from '../Context';
+import { HistoricalChart } from '../config/api';
+import axios from 'axios';
+import { CircularProgress, createTheme, ThemeProvider, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
+import { Line } from 'react-chartjs-2';
 
-const CoinInfo = () => {
+// export const options = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+//       position: 'top',
+//     },
+//     title: {
+//       display: true,
+//       text: 'chart!'
+//     }
+//   }
+// };
+
+
+const CoinInfo = ({ coin }) => {
+  const [historicalData, setHistoricalData] = useState();
+  const [days, setDays] = useState(1);
+
+  const { currency } = AppState();
+
+  const theme = useTheme();
+
+  const fetchHistoricData = async() => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setHistoricalData(data.prices);
+  }
+
+  console.log(historicalData);
+
+  useEffect(() => {
+    fetchHistoricData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency, days])
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark'
+    },
+  });
+
+  // const labels = [historicalData.map((coin) => {
+  //   let date = new Date(coin[0]);
+  //   let time = date.getHours() > 12
+  //     ? `${date.getHours() - 12}: ${date.getMinutes()} PM`
+  //     : `${date.getHours()}: ${date.getMinutes()} AM`;
+
+  //   return days === 1
+  //     ? time
+  //     : date.toLocaleDateString();})]
+  
+  // const data = {
+  //   labels,
+  //   datasets: [historicalData.map((coin) => coin[1])]
+  // }
+
   return (
-    <div>CoinInfo</div>
+    <ThemeProvider theme={darkTheme}>
+      <Box
+        sx={{
+          width: '75%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          mt: 25,
+          p: 40,
+          [theme.breakpoints.down('md')]: {
+            width: '100%',
+            mt: 0,
+            p: 20,
+            pt: 0,
+          }
+        }}
+      >
+        {/* {!historicalData ? (
+          <CircularProgress
+            style={{color: '#F7AB8A'}}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              options={options}
+              data={data}
+            />
+          </>
+        )} */}
+
+      </Box>
+    </ThemeProvider>
   )
 }
 
--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -3,27 +3,50 @@ import { useParams } from 'react-router-dom'
 import { AppState } from '../Context';
 import { SingleCoin } from '../config/api';
 import axios from 'axios';
-import { Box, useTheme } from '@mui/material';
+import { Box, LinearProgress, Typography, useTheme } from '@mui/material';
 import CoinInfo from '../components/CoinInfo';
+import parse from 'html-react-parser';
+import { numberWithCommas } from '../components/Carousel';
 
 const Coin = () => {
   const { id } = useParams();
-  const [coin, setcoin] = useState();
+  const [coin, setCoin] = useState();
 
   const { currency, symbol } = AppState();
 
   const theme = useTheme();
 
+  const marketData = {
+    alignSelf: 'start',
+    p: 5,
+    pt: 10,
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      display: 'flex',
+      justifyContent: 'space-around',
+    },
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    [theme.breakpoints.down('xs')]: {
+      alignItems: 'start',
+    }
+  }
+
   const fetchCoin = async() => {
     const { data } = await axios.get(SingleCoin(id));
-    setcoin(data);
+    setCoin(data);
   }
 
   // console.log(coin);
 
   useEffect(() => {
     fetchCoin();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (!coin) return <LinearProgress style={{backgroundColor: '#F7AB8A'}} />;
 
   return (
     <Box
@@ -55,6 +78,53 @@ const Coin = () => {
           height='200'
           style={{marginBottom: 20}}
         />
+        <Typography
+          variant='h3'
+          style={{
+            fontWeight: 'bold',
+            marginBottom: 20,
+            fontFamily: 'Montserrat',
+          }}
+        >
+          {coin?.name}
+        </Typography>
+        <Typography variant=''>
+          {parse(String(coin?.description.en.split('.')[0]))}.
+        </Typography>
+        {/* MARKET DATA */}
+        <Box sx={marketData}>
+          <span style={{display: 'flex'}}>
+            <Typography variant='h5' style={{fontFamily: 'Montserrat'}}>
+              Rank:
+            </Typography>
+            <Typography variant='h5' style={{fontFamily: 'Montserrat'}}>
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+          <span style={{display: 'flex'}}>
+            <Typography variant='h5' style={{fontFamily: 'Montserrat'}}>
+              Current Price:
+            </Typography>
+            <Typography variant='h5' style={{fontFamily: 'Montserrat'}}>
+              {symbol} {' '}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span style={{display: 'flex'}}>
+            <Typography variant='h5' style={{fontFamily: 'Montserrat'}}>
+              Market Cap:
+            </Typography>
+            <Typography variant='h5' style={{fontFamily: 'Montserrat'}}>
+              {symbol} {" "}
+              {numberWithCommas(
+                coin?.market_data.market_cap[currency.toLowerCase()]
+                  .toString()
+              )}
+            </Typography>
+          </span>
+        </Box>
       </Box>
       <CoinInfo coin={coin}/>
     </Box>
